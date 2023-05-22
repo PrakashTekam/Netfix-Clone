f7a874fb66d0de68e61cec8260363b953676148c
Prakash Tekam <98883758+PrakashTekam@users.noreply.github.com>
2023-05-22 19:28:58 +0530

    Add files via upload

---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+
+  return (
+    
+    <div className="nav">
+      <img className="nav__logo" src="https://www.techdaily.com.au/b2/wp-content/uploads/2015/03/Netflix_Logo_Print_FourColorCMYK.png" alt="Netflix Logo"/>
+    
+
+      <img className="nav__avatar" src="https://avatarfiles.alphacoders.com/971/thumb-1920-97156.png" alt="Netflix Logo"/>
+    
+    </div>
+    
+  )
+}
+
+export default Nav
---
+++ src/apikey.js
@@ -0,0 +1,3 @@
+ const  apikey = "fbef1f5b16f09f4e6b16350473743269";
+
+ const mymovieapi = "https://api.themoviedb.org/3/movie/550?api_key=fbef1f5b16f09f4e6b16350473743269";
